59ea2bcd23bfbd4c024394ea9449c86ffc0bf783
Krystian <96619815+Avsh11@users.noreply.github.com>
2026-06-06 09:42:03 +0200

    feat: testowy scanf
    Przy okazji powtorka przed kolosem
    formatowanie i komentarze wiadomo
    wszystko niepoprawnie ale bedzie zmieniane

--- rozwiniecie.asm
+++ rozwiniecie.asm
@@ -2,35 +2,89 @@
 
 ;        esp -> [ret]  ; ret - adres powrotu do asmloader
 
-licznik   equ 1
-mianownik equ 17
-n         equ 15
+%ifdef   COMMENT
+na razie bez analiz stosu i kom do getaddr
+(nie mam home na klawiaturze nie chce mi sie kopiowac)
++ maly ekran
+%endif
 
-         push mianownik  ; mianownik -> stack
-         push licznik    ; licznik -> stack
+max_digit equ 63
 
-;        esp -> [licznik][mianownik][n][ret]
+         sub esp, 16  ; esp = esp - 16
+         
+         mov ebp, esp  ; ebp = adres licznika
+         
+         call getaddr_pl  ; (pl) prompt licznik
+format_pl:
+         db "licznik = ", 0
+getaddr_pl:
 
-         call getaddr_x  ; push on the stack the run-time address of format and jump to getaddr
-format_x:
-         db "x = %d/%d", 0xA, 0
-getaddr_x:
+         call [ebx+3*4]  ; printf(format_pl)
+         
+         lea eax, [ebp+0]  ; eax - adres licznika
+         
+         push eax  ; eax -> stack
+         
+         call getaddr_sl  ; scanf licznik
+format_sl:
+         db "%d", 0
+getaddr_sl:
+
+         call [ebx+4*4]
+         add esp, 2*4
+
+         call getaddr_pm  ; prompt mianownik
+format_pm:
+         db "mianownik = ", 0
+getaddr_pm:
 
          call [ebx+3*4]
-         add esp, 3*4
          
-         push n
+         lea eax, [ebp+4]  ; eax = adres mianownika
+
+         push eax  ; eax -> stack
          
-         call getaddr_n
-format_n:
-         db "n = %d", 0xA, 0
-getaddr_n:
+         call getaddr_sm
+format_sm:
+         db "%d", 0
+getaddr_sm:
+
+         call [ebx+4*4]
+         add esp, 2*4
+         
+         call getaddr_pn
+format_pn:
+          db "n = ", 0
+getaddr_pn:
+
+           call [ebx+3*4]
+           
+           lea eax, [ebp+8]
+           
+           push eax
+           
+           call getaddr_sn
+format_sn:
+          db "%d", 0
+getaddr_sn:
+
+           call [ebx+4*4]
+           add esp, 2*4
+
+           push dword [ebp+8]  ; n -> stack
+           push dword [ebp+4]   ; mianownik -> stack
+           push dword [ebp+0]   ; licznik -> stack
+
+           call getaddr
+format:
+       db "wczytano: %d / %d, n = %d", 0xA, 0
+getaddr:
 
          call [ebx+3*4]  ; printf(format, licznik, mianownik, n);
-         add esp, 2*4    ; esp = esp + 8
+         add esp, 4*4    ; esp = esp + 8
 
 ;        esp -> [ret]
-
+         add esp, 16  ; zwolnic blok danych
          push 0          ; esp -> [00 00 00 00][ret]
          call [ebx+0*4]  ; exit(0);
 
